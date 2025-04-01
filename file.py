d8650ee6e41a70675b6d7b84062a5f1e0aea3896
Daniyil Preen <daniyil.aurora@gmail.com>
2025-04-01 19:20:28 +0200

    Added organisation by creation date

--- file.py
+++ file.py
@@ -1,15 +1,18 @@
 class File():
     def __init__(self, fileName: str, filePath: str):
         self.fileName = fileName
-        self.filePath = filePath.replace("\\", "/") # To make directory valid
+        self.filePath = filePath.replace("\\", "/")  # To make directory valid
         self.extension = fileName.split(".")
-        self.extension = self.extension[len(self.extension) - 1] # Get file extension
-    
+        self.extension = self.extension[len(self.extension) - 1]  # Get file extension
+
     def getName(self):
         return self.fileName
+
     def getPath(self):
         return self.filePath
+
     def getExtension(self):
         return self.extension
+
     def __str__(self):
         return (f'FileName: {self.fileName}, Extension: {self.extension}, FilePath: {self.filePath}')
--- main.py
+++ main.py
@@ -1,42 +1,65 @@
 import os
 import shutil
 import file
+import datetime
 
 # Getting all files in specific directory
 def getFiles(dir: str):
     files = []
     for (dirPath, dirNames, fileNames) in os.walk(dir):
         for fileName in fileNames:
+            if not dirPath[len(dirPath) - 1] == "/":
+                dirPath = dirPath + "/"  # Add symbol '/' at the end of the path
+
             newFile = file.File(fileName, dirPath)
             files.append(newFile)
-    
+
     return files
 
-# Dividing files into categories
-def categoriseFiles(files: list[file.File]):
+
+# Dividing files into categories by their filetype
+def categoriseFilesByType(files: list[file.File]):
     categories = {}
     for newFile in files:
-            extension = newFile.getExtension()
+            extension = newFile.getExtension()  # Get file creation date
 
-            if not extension in categories:
-                categories[extension] = list() # Create new list for files in category
+            if extension not in categories:
+                categories[extension] = list()  # Create new list for files in category
 
-            categories[extension].append(newFile)
-    
+            categories[extension].append(newFile)  # Add file to specific category
+
     return categories
+
+
+# Dividing files into categories by their date of creation
+def categoriseFilesByDate(files: list[file.File]):
+    categories = {}
+
+    for newFile in files:
+        creationDate = os.path.getctime(newFile.filePath + newFile.fileName)  # Get file creation time
+        creationDate = datetime.datetime.fromtimestamp(creationDate)
+
+        if not f'{creationDate.day}.{creationDate.month}.{creationDate.year}' in categories:
+            categories[f'{creationDate.day}.{creationDate.month}.{creationDate.year}'] = list()  # Create new list for files in category
+
+        categories[f'{creationDate.day}.{creationDate.month}.{creationDate.year}'].append(newFile)  # Add file to specific category
+
+    return categories
+
 
 # Moving files from one folder to another
 def moveFiles(categories: dict, newPath: str):
     for category in categories:
         newFolder = os.mkdir(f'{newPath}/{category}')
         for newFile in categories[category]:
-            shutil.copy(f'{newFile.getPath()}/{newFile.getName()}', f'{newPath}/{category}/{newFile.getName()}') # Copy files from old path to new
+            shutil.copy(f'{newFile.getPath()}/{newFile.getName()}', f'{newPath}/{category}/{newFile.getName()}')  # Copy files from old path to new
 
 
 def main():
     files = getFiles("./testData/testDir")
     newPath = "./testData/newTestDir"
-    categories = categoriseFiles(files)
+    categories = categoriseFilesByDate(files)
     moveFiles(categories, newPath)
+
 
 main()
